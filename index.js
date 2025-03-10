35be0a94885c09717b291e8bf62c39ca43247cf9
Kane Kabena <onekaneldn@gmail.com>
2025-03-10 08:30:04 +0300

    First Class functions continued

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+console.log(selectingDrivers[0](['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+console.log(selectingDrivers[1](['Antonia', 'Nuru', 'Amari', 'Mo'])); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+
+const fareQuadrupler = createFareMultiplier(4);
+console.log(fareQuadrupler(10)); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const fareDoubler = createFareMultiplier(2);
+console.log(fareDoubler(10)); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const fareTripler = createFareMultiplier(3);
+console.log(fareTripler(10)); 
+
+//------------------------------------------------------------------------------------------------------------
+
+const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+};
+
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)); 
+
+console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers));  
